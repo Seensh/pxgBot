52293c392e066095b5003205d55def5d222f487a
Felipe <felipe.barbosa001@hotmail.com>
2022-05-03 16:00:55 -0300

    atualizacao bot caverna

--- auxiliar.py
+++ auxiliar.py
@@ -1,13 +1,4 @@
 import pyautogui
 import time
-from datetime import datetime
 
-now = datetime.now()
- 
-pizza = pyautogui.locateOnScreen('pizza.png', grayscale=True,confidence=0.8)
-pizzaCenter = pyautogui.center(pizza)
-pyautogui.click(pizzaCenter.x,pizzaCenter.y,button='right')
-poke = pyautogui.locateOnScreen('poke.png', grayscale=True,confidence=0.8)
-pokeCenter = pyautogui.center(poke)
-pyautogui.click(pokeCenter.x,pokeCenter.y)
 pyautogui.displayMousePosition()
---
+++ interfaceBot/index.py
@@ -0,0 +1,19 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario')],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+
+janela = sg.Window('Tela de Login', layout)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'felipe' and valores ['senha'] == '123456':
+            print('Bem-vindo ao bot vinisesc')
