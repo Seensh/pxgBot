758abdce87f9512abd57e2a403d130159a2fc0cc
Felipe <felipe.barbosa001@hotmail.com>
2022-05-04 15:26:42 -0300

    adicionando captura identificando pokemons batalha

--- auxiliar.py
+++ auxiliar.py
@@ -1,4 +1,27 @@
 import pyautogui
 import time
+from PIL import Image
+import numpy as np
 
-pyautogui.displayMousePosition()
+#pyautogui.displayMousePosition()
+
+positionBatalha = pyautogui.locateOnScreen('images/batalha/batalha.png', grayscale=True, confidence=0.7)
+if(positionBatalha):
+    positionCenterBatalha = pyautogui.center(positionBatalha)
+    positionX = positionCenterBatalha.x-45
+    positionY = positionCenterBatalha.y+78
+    largura = positionX-577
+    altura = positionY-490
+    scBatalha = pyautogui.screenshot(region=(positionX,positionY,largura,altura))
+    scBatalha.save('images/batalha/batalhaCheia.png')
+
+positionBatalhaVazia = pyautogui.locateOnScreen('images/batalha/batalhaCheia.png', grayscale=True, confidence=0.7)
+
+if(positionBatalhaVazia):
+    print("Vazia")
+else:
+    print("Tem algo")
+
+
+
+
--- index.py
+++ index.py
@@ -9,7 +9,6 @@ def functionMain():
         positionVara = None
         positionAgua = None
         positionAguaBorbulhando = None
-        positionNameMagi = None
         positionHabilidades = None 
         count = 0
         time.sleep(1)    
@@ -36,32 +35,31 @@ def functionMain():
         pyautogui.click(positionCenterAgua.x,positionCenterAgua.y)
         pyautogui.moveTo((positionCenterAgua.x),(positionCenterAgua.y+20))
         print('Agua')
-
         time.sleep(2)
+        positionNaoHaRota = pyautogui.locateOnScreen('images/naoHaRota.png', grayscale=True,confidence=0.7)
+        if(positionNaoHaRota != None):
+            functionRepetirJogarVara()
         #CAPTURA POSIÇÃO AGUA BORBULHANDO
         while (positionAguaBorbulhando == None):
             positionAguaBorbulhando = pyautogui.locateOnScreen('aguaBorbulhando.png', grayscale=True,confidence=0.8)
         pyautogui.click(positionCenterVara.x,positionCenterVara.y)
         pyautogui.moveTo((positionCenterVara.x),(positionCenterVara.y+20))
         print('Agua Borbulhando')
-        time.sleep(2)
-
-        #CAPTURA POSIÇÃO Magikarp
-
-        while (positionNameMagi == None):      
-            positionNameMagi = pyautogui.locateOnScreen('nameMagi.png', grayscale=True,confidence=0.8)
-            count = count + 1
-            if(count > 5):
-                positionNameMagi = None
-        if(positionNameMagi != None):
-            positionCenterNameMagi = pyautogui.center(positionNameMagi)
-            #pyautogui.click(positionCenterNameMagi.x,positionCenterNameMagi.y)
-            print('Magikarp')
-        time.sleep(2)
-
+        time.sleep(0.8)
+        positionBatalha = pyautogui.locateOnScreen('images/batalha/batalha.png', grayscale=True, confidence=0.7)
+        print('Tirando foto dos pokemons')
+        if(positionBatalha):
+            positionCenterBatalha = pyautogui.center(positionBatalha)
+            positionX = positionCenterBatalha.x-45
+            positionY = positionCenterBatalha.y+78
+            largura = positionX-577
+            altura = positionY-490
+            scBatalha = pyautogui.screenshot(region=(positionX,positionY,largura,altura))
+            scBatalha.save('images/batalha/batalhaCheia.png')
+            print('Tirei foto')
         #CAPTURA POSIÇÃO Hanilidades
         positionHabilidades = pyautogui.locateOnScreen('habilidades.png', grayscale=True)
-        while(positionNameMagi != None):
+        while(positionBatalha != None):
             if(positionHabilidades != None):
                 positionCenterHabilidades = pyautogui.center(positionHabilidades)
                 time.sleep(1)
@@ -70,14 +68,49 @@ def functionMain():
                 pyautogui.click((positionCenterHabilidades.x + 80),positionCenterHabilidades.y)
                 time.sleep(2)
                 pyautogui.click((positionCenterHabilidades.x + 120),positionCenterHabilidades.y)
-                time.sleep(1)                
-            positionNameMagi = pyautogui.locateOnScreen('nameMagi.png', grayscale=True,confidence=0.8)
-
-            print('Habilidades')
+                time.sleep(1)     
+                print('Habilidades')           
+            positionBatalha = pyautogui.locateOnScreen('images/batalha/batalhaCheia.png', grayscale=True, confidence=0.7)
+            print("comparando se existe pokemons")
+                
+            
+        print("Não há mais pokemons")
+        positionDeadDMagikarp = pyautogui.locateOnScreen('pokemon.png', grayscale=True, confidence=0.7)
+        time.sleep(0.5)
+        while(positionDeadDMagikarp != None):
+            if (positionDeadDMagikarp != None):
+                positionSuperball = pyautogui.locateOnScreen('images/ball/ultra.png',confidence=0.7)
+                time.sleep(1)
+                if(positionSuperball != None):
+                    positionCenterSuperball = pyautogui.center(positionSuperball)
+                    positionCenterDeadMagikarp = pyautogui.center(positionDeadDMagikarp)
+                    time.sleep(0.5)
+                    pyautogui.click(positionCenterSuperball.x,positionCenterSuperball.y,button='right')
+                    time.sleep(1)
+                    pyautogui.click(positionCenterDeadMagikarp.x,positionCenterDeadMagikarp.y)
+                    time.sleep(3)
+            positionDeadDMagikarp = pyautogui.locateOnScreen('pokemon.png', grayscale=True, confidence=0.7)
+        print('Estou capturando')
 
 def functionPositionVara(positionVara):
     positionVara = pyautogui.locateOnScreen('vara.png')
     return positionVara
 
+def functionRepetirJogarVara():
+    #CAPTURA POSIÇÃO VARA
+    while (positionVara == None):
+        positionVara = pyautogui.locateOnScreen('vara.png', grayscale=True,confidence=0.8)
+    positionCenterVara = pyautogui.center(positionVara)
+    pyautogui.click(positionCenterVara.x,positionCenterVara.y)
+    pyautogui.moveTo((positionCenterVara.x),(positionCenterVara.y+20))
+    print('Vara')
+    time.sleep(2)
+    #CAPTURA POSIÇÃO AGUA
+    while (positionAgua == None):
+        positionAgua = pyautogui.locateOnScreen('agua.png', grayscale=True,confidence=0.8)
+    positionCenterAgua = pyautogui.center(positionAgua)
+    pyautogui.click(positionCenterAgua.x,positionCenterAgua.y)
+    pyautogui.moveTo((positionCenterAgua.x),(positionCenterAgua.y+20))
+    print('Agua')
 
 functionMain()
